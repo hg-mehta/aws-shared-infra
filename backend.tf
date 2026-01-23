cae513d82a6846f344dcfca39f1e6d81c7c4b407
hg-mehta <harsh_m@hotmail.com>
2026-01-22 21:49:01 -0500

    Update the backend S3 bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "hg-mehta-shared-infra-state-us-east-1"
+    bucket         = "hgmehta-shared-infra-state-us-east-1"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
